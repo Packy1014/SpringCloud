71315b343ad039a5e2e0f0753a60213484e79c9f
Packy <packy1014@yahoo.co.jp>
2018-10-29 22:43:26 +0900

    Add eureka server and eureka client

---
+++ eureka_client/README.md
@@ -0,0 +1,2 @@
+http://localhost:8771/api/v1/products/
+http://localhost:8771/api/v1/products/1
---
+++ eureka_client/src/main/java/com/packy/springcloud/eureka_client/service/impl/ProductServiceImpl.java
@@ -0,0 +1,36 @@
+package com.packy.springcloud.eureka_client.service.impl;
+
+import com.packy.springcloud.eureka_client.domain.Product;
+import com.packy.springcloud.eureka_client.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+
+    private final static Map<Integer, Product> productMap;
+
+    static {
+        productMap = new HashMap<>();
+        productMap.put(1, new Product(1, "Product_1", 10, 100));
+        productMap.put(2, new Product(2, "Product_2", 20, 200));
+        productMap.put(3, new Product(3, "Product_3", 30, 300));
+        productMap.put(4, new Product(4, "Product_4", 40, 400));
+        productMap.put(5, new Product(5, "Product_5", 50, 500));
+        productMap.put(6, new Product(6, "Product_6", 60, 600));
+    }
+
+    @Override
+    public List<Product> listProducts() {
+        return new ArrayList<>(productMap.values());
+    }
+
+    @Override
+    public Product findById(int id) {
+        return productMap.get(id);
+    }
+}
